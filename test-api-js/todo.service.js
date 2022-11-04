eddd7a9eff499e6fc0cc389c66cbc56ef6f937c3
Shaliq Nigal <shaliqnigal.savudinnayanveetil@slu.edu>
2022-11-04 13:15:53 -0500

    Implemented all the functions i.e Add, Delete and Update

--- test-api-js/todo.service.js
+++ test-api-js/todo.service.js
@@ -24,16 +24,28 @@ class todoservice{
 
     add_todo(todo){
         // Your code here
+        this.todo_data.todo.push(todo);
+        console.log(this.todo_data.todo);
     }
 
     delete_todo(id){
         // Your code here
+        this.todo_data.todo.splice(id-1,1)
+        console.log("Deleting element ");
+        console.log(this.todo_data.todo);
     }
 
     update_todo(id, todo){
         // Your code here
+        this.todo_data.todo[id-1] = todo;
+        console.log("Updating element ");
+        console.log(this.todo_data.todo);
     }
 }
 
+const a = new todoservice()
+a.add_todo({"title":"T4","description":"D4","done":false});
+a.delete_todo(1)
+a.update_todo(1,{"title":"T2","description":"D2","done":true})
 
 module.exports= todoservice;
