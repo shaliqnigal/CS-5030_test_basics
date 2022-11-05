110577fe6b5d5c888e8501f26baa8c1814b93c3b
Shaliq Nigal <shaliqnigal.savudinnayanveetil@slu.edu>
2022-11-05 16:20:08 -0500

    Added the additional test case - add_todos_check_element for the add function

--- test-api-js/todo.test.js
+++ test-api-js/todo.test.js
@@ -23,21 +23,30 @@ describe('todo test suite', () => {
 
     // Write all your test cases here that corresponds to software requirements
 
-    // Test to check if the length of the todo list is reduced from 3 to 4 after deleting an item
-    test("add_todos", () => {
-        expect(todo_service.add_todo({"title":"T4","description":"D4","done":false}).todo.length).toEqual(4);
+    // Test to check if the length of the todo list is increased from 4 to 3 after inserting an item
+    test("add_todos_check_list_length", () => {
+        let result = todo_service.add_todo({"title":"T4","description":"New item added - D4","done":false});
+        expect(result.todo.length).toEqual(4);
+
     });
 
-    // Test to check if the length of the todo list is reduced from 4 to 3 after deleting an item
+    // Test to check if the item is inserted at the right index after inserting an item
+    test("add_todos_check_element", () => {
+        let result = todo_service.add_todo({"title":"T5","description":"New item added - D5","done":false});
+        expect(result.todo[4]).toEqual({ title: 'T5', description: 'New item added - D5', done: false });
+    });
+
+    // Test to check if the length of the todo list is reduced from 5 to 4 after deleting an item
     test("delete_todos", () => {
-        expect(todo_service.delete_todo(4).todo.length).toEqual(3);
+        let result = todo_service.delete_todo(4)
+        expect(result.todo.length).toEqual(4);
     });
 
     // Test to check if the to do item is updated in the right index
     test("update_todos", () => {
-        let result = todo_service.update_todo(1,{"title":"T2","description":"D2-updated","done":true})
+        let result = todo_service.update_todo(1,{"title":"T2-UPDATED","description":"D2-updated","done":true})
         //console.log(result.todo[0]);
-        expect(result.todo[0]).toEqual({ title: 'T2', description: 'D2-updated', done: true });
+        expect(result.todo[0]).toEqual({ title: 'T2-UPDATED', description: 'D2-updated', done: true });
     });
 
 });
